cc1942d0f92f66970431760913af79da45bb695b
Harold D. Turyasingura <tharoldd@gmail.com>
2024-09-14 12:38:55 +0300

    java lsp now sort of works

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,3 +1,4 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -21,6 +22,7 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
+          "java",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
